485a28c5916af88ea853a38bf848c044bd041001
Parker Eastman <ParkerEastman@outlook.com>
2019-04-13 08:28:26 -0400

    Contained rouletteWheel list within constructor.  Created random object to call rouletteElements

--- Roulette/Options.cs
+++ Roulette/Options.cs
@@ -24,50 +24,61 @@ namespace Roulette
 
     public class RouletteWheel {
 
+        public List<RouletteElement> rouletteWheel;
 
-        RouletteElement Zero = new RouletteElement("0", 0, "Green");
-        RouletteElement ZeroZero = new RouletteElement("00", 0, "Green");
-        RouletteElement One = new RouletteElement("1", 1, "Red");
-        RouletteElement Two = new RouletteElement("2", 2, "Black");
-        RouletteElement Three = new RouletteElement("3", 3, "Red");
-        RouletteElement Four = new RouletteElement("4", 4, "Black");
-        RouletteElement Five = new RouletteElement("5", 5, "Red");
-        RouletteElement Six = new RouletteElement("6", 6, "Black");
-        RouletteElement Seven = new RouletteElement("7", 7, "Red");
-        RouletteElement Eight = new RouletteElement("8", 8, "Black");
-        RouletteElement Nine = new RouletteElement("9", 9, "Red");
-        RouletteElement Ten = new RouletteElement("10", 10, "Black");
-        RouletteElement Eleven = new RouletteElement("11", 11, "Black");
-        RouletteElement Twelve = new RouletteElement("12", 12, "Red");
-        RouletteElement Thirteen = new RouletteElement("13", 13, "Black");
-        RouletteElement Fourteen = new RouletteElement("14", 14, "Red");
-        RouletteElement Fifteen = new RouletteElement("15", 15, "Black");
-        RouletteElement Sixteen = new RouletteElement("16", 16, "Red");
-        RouletteElement Seventeen = new RouletteElement("17", 17, "Black");
-        RouletteElement Eighteen = new RouletteElement("18", 18, "Red");
-        RouletteElement Nineteen = new RouletteElement("19", 19, "Red");
-        RouletteElement Twenty = new RouletteElement("20", 20, "Black");
-        RouletteElement TwentyOne = new RouletteElement("21", 21, "Red");
-        RouletteElement TwentyTwo = new RouletteElement("22", 22, "Black");
-        RouletteElement TwentyThree = new RouletteElement("23", 23, "Red");
-        RouletteElement TwentyFour = new RouletteElement("24", 24, "Black");
-        RouletteElement TwentyFive = new RouletteElement("25", 25, "Red");
-        RouletteElement TwentySix = new RouletteElement("26", 26, "Black");
-        RouletteElement TwentySeven = new RouletteElement("27", 27, "Red");
-        RouletteElement TwentyEight = new RouletteElement("28", 28, "Black");
-        RouletteElement TwentyNine = new RouletteElement("29", 29, "Black");
-        RouletteElement Thirty = new RouletteElement("30", 30, "Red");
-        RouletteElement ThirtyOne = new RouletteElement("31", 31, "Black");
-        RouletteElement ThirtyTwo = new RouletteElement("32", 32, "Red");
-        RouletteElement ThirtyThree = new RouletteElement("33", 33, "Black");
-        RouletteElement ThirtyFour = new RouletteElement("34", 34, "Red");
-        RouletteElement ThirtyFive = new RouletteElement("35", 35, "Black");
-        RouletteElement ThirtySix = new RouletteElement("36", 36, "Red");
+        readonly RouletteElement Zero = new RouletteElement("0", 0, "Green");
+        readonly RouletteElement ZeroZero = new RouletteElement("00", 0, "Green");
+        readonly RouletteElement One = new RouletteElement("1", 1, "Red");
+        readonly RouletteElement Two = new RouletteElement("2", 2, "Black");
+        readonly RouletteElement Three = new RouletteElement("3", 3, "Red");
+        readonly RouletteElement Four = new RouletteElement("4", 4, "Black");
+        readonly RouletteElement Five = new RouletteElement("5", 5, "Red");
+        readonly RouletteElement Six = new RouletteElement("6", 6, "Black");
+        readonly RouletteElement Seven = new RouletteElement("7", 7, "Red");
+        readonly RouletteElement Eight = new RouletteElement("8", 8, "Black");
+        readonly RouletteElement Nine = new RouletteElement("9", 9, "Red");
+        readonly RouletteElement Ten = new RouletteElement("10", 10, "Black");
+        readonly RouletteElement Eleven = new RouletteElement("11", 11, "Black");
+        readonly RouletteElement Twelve = new RouletteElement("12", 12, "Red");
+        readonly RouletteElement Thirteen = new RouletteElement("13", 13, "Black");
+        readonly RouletteElement Fourteen = new RouletteElement("14", 14, "Red");
+        readonly RouletteElement Fifteen = new RouletteElement("15", 15, "Black");
+        readonly RouletteElement Sixteen = new RouletteElement("16", 16, "Red");
+        readonly RouletteElement Seventeen = new RouletteElement("17", 17, "Black");
+        readonly RouletteElement Eighteen = new RouletteElement("18", 18, "Red");
+        readonly RouletteElement Nineteen = new RouletteElement("19", 19, "Red");
+        readonly RouletteElement Twenty = new RouletteElement("20", 20, "Black");
+        readonly RouletteElement TwentyOne = new RouletteElement("21", 21, "Red");
+        readonly RouletteElement TwentyTwo = new RouletteElement("22", 22, "Black");
+        readonly RouletteElement TwentyThree = new RouletteElement("23", 23, "Red");
+        readonly RouletteElement TwentyFour = new RouletteElement("24", 24, "Black");
+        readonly RouletteElement TwentyFive = new RouletteElement("25", 25, "Red");
+        readonly RouletteElement TwentySix = new RouletteElement("26", 26, "Black");
+        readonly RouletteElement TwentySeven = new RouletteElement("27", 27, "Red");
+        readonly RouletteElement TwentyEight = new RouletteElement("28", 28, "Black");
+        readonly RouletteElement TwentyNine = new RouletteElement("29", 29, "Black");
+        readonly RouletteElement Thirty = new RouletteElement("30", 30, "Red");
+        readonly RouletteElement ThirtyOne = new RouletteElement("31", 31, "Black");
+        readonly RouletteElement ThirtyTwo = new RouletteElement("32", 32, "Red");
+        readonly RouletteElement ThirtyThree = new RouletteElement("33", 33, "Black");
+        readonly RouletteElement ThirtyFour = new RouletteElement("34", 34, "Red");
+        readonly RouletteElement ThirtyFive = new RouletteElement("35", 35, "Black");
+        readonly RouletteElement ThirtySix = new RouletteElement("36", 36, "Red");
 
 
-        List<RouletteElement> rouletteWheel = new List<RouletteElement>();
+        public RouletteWheel()
+        {
+            rouletteWheel = new List<RouletteElement>
+            {
+                Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+                Eleven, Twelve, Thirteen, Fourteen, Fifteen, Sixteen, Seventeen, Eighteen, Nineteen, Twenty,
+                TwentyOne, TwentyTwo, TwentyThree, TwentyFour, TwentyFive, TwentySix, TwentySeven, TwentyEight, TwentyNine, Thirty,
+                ThirtyOne, ThirtyTwo, ThirtyThree, ThirtyFour, ThirtyFive, ThirtySix, ZeroZero
+            };
+        }
 
-        
+
+
 
 
 
--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -16,56 +16,15 @@ namespace Roulette
 
         static void doWork()
         {
-            RouletteElement someElement;
+            Random rand = new Random();
+            rand.Next(0, 37);
 
-            RouletteElement Zero = new RouletteElement("0", 0, "Green");
-            RouletteElement ZeroZero = new RouletteElement("00", 0, "Green");
-            RouletteElement One = new RouletteElement("1", 1, "Red");
-            RouletteElement Two = new RouletteElement("2", 2, "Black");
-            RouletteElement Three = new RouletteElement("3", 3, "Red");
-            RouletteElement Four = new RouletteElement("4", 4, "Black");
-            RouletteElement Five = new RouletteElement("5", 5, "Red");
-            RouletteElement Six = new RouletteElement("6", 6, "Black");
-            RouletteElement Seven = new RouletteElement("7", 7, "Red");
-            RouletteElement Eight = new RouletteElement("8", 8, "Black");
-            RouletteElement Nine = new RouletteElement("9", 9, "Red");
-            RouletteElement Ten = new RouletteElement("10", 10, "Black");
-            RouletteElement Eleven = new RouletteElement("11", 11, "Black");
-            RouletteElement Twelve = new RouletteElement("12", 12, "Red");
-            RouletteElement Thirteen = new RouletteElement("13", 13, "Black");
-            RouletteElement Fourteen = new RouletteElement("14", 14, "Red");
-            RouletteElement Fifteen = new RouletteElement("15", 15, "Black");
-            RouletteElement Sixteen = new RouletteElement("16", 16, "Red");
-            RouletteElement Seventeen = new RouletteElement("17", 17, "Black");
-            RouletteElement Eighteen = new RouletteElement("18", 18, "Red");
-            RouletteElement Nineteen = new RouletteElement("19", 19, "Red");
-            RouletteElement Twenty = new RouletteElement("20", 20, "Black");
-            RouletteElement TwentyOne = new RouletteElement("21", 21, "Red");
-            RouletteElement TwentyTwo = new RouletteElement("22", 22, "Black");
-            RouletteElement TwentyThree = new RouletteElement("23", 23, "Red");
-            RouletteElement TwentyFour = new RouletteElement("24", 24, "Black");
-            RouletteElement TwentyFive = new RouletteElement("25", 25, "Red");
-            RouletteElement TwentySix = new RouletteElement("26", 26, "Black");
-            RouletteElement TwentySeven = new RouletteElement("27", 27, "Red");
-            RouletteElement TwentyEight = new RouletteElement("28", 28, "Black");
-            RouletteElement TwentyNine = new RouletteElement("29", 29, "Black");
-            RouletteElement Thirty = new RouletteElement("30", 30, "Red");
-            RouletteElement ThirtyOne = new RouletteElement("31", 31, "Black");
-            RouletteElement ThirtyTwo = new RouletteElement("32", 32, "Red");
-            RouletteElement ThirtyThree = new RouletteElement("33", 33, "Black");
-            RouletteElement ThirtyFour = new RouletteElement("34", 34, "Red");
-            RouletteElement ThirtyFive = new RouletteElement("35", 35, "Black");
-            RouletteElement ThirtySix = new RouletteElement("36", 36, "Red");
+            RouletteWheel rwInst = new RouletteWheel();
 
-            RouletteElement[] rouletteWheel = {Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
-                Eleven, Twelve, Thirteen, Fourteen, Fifteen, Sixteen, Seventeen, Eighteen, Nineteen, Twenty,
-                TwentyOne, TwentyTwo, TwentyThree, TwentyFour, TwentyFive, TwentySix, TwentySeven, TwentyEight, TwentyNine, Thirty,
-                ThirtyOne, ThirtyTwo, ThirtyThree, ThirtyFour, ThirtyFive, ThirtySix, ZeroZero
-            };
 
             WinningOptions winOpts = new WinningOptions();
 
-            winOpts.SplitTest(rouletteWheel[1]);
+            winOpts.SplitTest(rwInst.rouletteWheel[rand.Next(0, 37)]);
 
 
 
